013130bc0c72e7ee17d5d89e2b06f0efbba862b6
Parakh42 <mahajan.pa@husky.neu.edu>
2019-04-17 21:34:07 -0400

    Step2:- Discount offer implemented

--- PeopleFluentExcericise/src/peoplefluentexcericise/PeopleFluentExcericise.java
+++ PeopleFluentExcericise/src/peoplefluentexcericise/PeopleFluentExcericise.java
@@ -5,8 +5,10 @@
  */
 package peoplefluentexcericise;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  *
@@ -19,36 +21,74 @@ public class PeopleFluentExcericise {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        String[] input = new String[]{"Apple","Orange","Orange","Apple"};
-        Double appleUnitCost = 0.60   ;
-        Double orangeUnitCost = 0.25;
-        HashMap<String,Integer> hmap = new HashMap<String,Integer>();
-        
-        if(input.length == 0){
-            System.out.println("You cart is empty");
+        System.out.println("Enter comma separated list of fruits(Apple/Orange):-");
+        Scanner userInput = new Scanner(System.in);
+        ArrayList<String> input = new ArrayList<String>();
+        String[] tokens = userInput.nextLine().split(",");
+        for(int i=0; i < tokens.length; i++){
+            input.add(tokens[i]);
         }
-        
-        for(int i = 0; i < input.length; i++){
-            if(hmap.containsKey(input[i])){
-                hmap.put(input[i], hmap.get(input[i]) + 1);
+        if(input.get(0).equals("")){
+            System.out.println("You cart is empty");
+            return;
+        }
+
+        Double appleUnitCost = 0.60;
+        Double orangeUnitCost = 0.25;
+
+        HashMap<String,Integer> hmap = new HashMap<String,Integer>();
+        for(int i = 0; i < input.size(); i++){
+            if(hmap.containsKey(input.get(i))){
+                hmap.put(input.get(i), hmap.get(input.get(i)) + 1);
             }
             else{
-                hmap.put(input[i], 1);
+                hmap.put(input.get(i), 1);
             }
         }
         Double total = 0.0;
+        Double discountedPrice = 0.0;
         for(Map.Entry<String, Integer> resMap : hmap.entrySet()){
-            if(resMap.getKey().equals("Apple")){
+            if(resMap.getKey().equalsIgnoreCase("Apple")){
+               discountedPrice +=  discountedOffer(resMap.getKey(),resMap.getValue());
                 total += resMap.getValue()*appleUnitCost;
             }
-            else{
+            else if(resMap.getKey().equalsIgnoreCase("Orange")){
+                discountedPrice +=  discountedOffer(resMap.getKey(),resMap.getValue());
                 total += resMap.getValue()*orangeUnitCost;
             }
         }
-        System.out.println(3/2);
-        System.out.println("Total amount is $" +total);
+        System.out.println("Total amount is $" + total );
+        Scanner dis = new Scanner(System.in);
+        System.out.println("Is the customer is eligible for discount(Y/N)?");
+        if(dis.nextLine().equalsIgnoreCase("Y")){
+            System.out.println("Discounted Amount is $" +discountedPrice);
+            total = total - discountedPrice;
+            System.out.println("Amount after discount $" + total);
+        }
         
+        
+        
+    }
+    
+    public static double discountedOffer(String fruitName, int quantity){
+        double discountedPrice = 0.0;
+        if(fruitName.equalsIgnoreCase("Apple")){
+            if(quantity < 2){
+               discountedPrice += 0.0;
+            }
+            else{
+               discountedPrice += (quantity/2) * 0.60;
+            }
+        }
+        else{
+            if(quantity < 3){
+               discountedPrice += 0.0;
+            }
+            else{
+                discountedPrice += (quantity/3) * 0.25;
+            }
+        }
+        return discountedPrice;
     }
     
 }
